d65a284df683757d8e539ae6ded7d095d2376f06
orBruhim <orbruhim@gmail.com>
2024-07-27 13:43:34 +0300

    add modal logic

---
+++ src/app/user-modal/user-modal.component.ts
@@ -0,0 +1,64 @@
+import {Component, inject, Inject, Input, model, OnInit} from '@angular/core';
+import {User} from "../shared/users.interface";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {
+    MAT_DIALOG_DATA,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle
+} from "@angular/material/dialog";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {CommonModule} from "@angular/common";
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+    selector: 'app-user-modal',
+    standalone: true,
+    imports: [
+        MatDialogContent,
+        MatFormField,
+        MatDialogTitle,
+        ReactiveFormsModule,
+        MatDialogActions,
+        MatInputModule,
+        CommonModule,
+        MatButtonModule,
+    ],
+    templateUrl: './user-modal.component.html',
+    styleUrl: './user-modal.component.scss'
+})
+export class UserModalComponent implements OnInit {
+    userForm: FormGroup;
+
+    constructor(
+        private fb: FormBuilder
+    ) {
+    }
+    readonly dialogRef = inject(MatDialogRef<UserModalComponent>);
+    readonly user = inject<User>(MAT_DIALOG_DATA);
+
+    ngOnInit() :void {
+        const urlRegex= '(https:\\/\\/www\\.|http:\\/\\/www\\.|https:\\/\\/|http:\\/\\/)?[a-zA-Z0-9]{2,}(\\.[a-zA-Z0-9]{2,})(\\.[a-zA-Z0-9]{2,})?';
+        const israeliPhoneRegex= '^\\+?(972|0)(\\-)?0?(([23489]{1}\\d{7})|[5]{1}\\d{8})$'
+        this.userForm = this.fb.group({
+            name: [this.user.name, [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+            email: [{ value: this.user.email, disabled: true }, Validators.required],
+            phone: [this.user.phone, [Validators.required, Validators.pattern(israeliPhoneRegex)]],
+            website: [this.user.website, Validators.pattern(urlRegex)]
+        });
+    }
+
+    saveUser() :void {
+        if (this.userForm.valid) {
+            // Handle saving user data here- Send PUT request when there is a real BE
+            this.close()
+        }
+    }
+
+    close() :void {
+        this.dialogRef.close();
+    }
+}
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -3,29 +3,46 @@ import {CommonModule, TitleCasePipe} from "@angular/common";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {UsersService} from "../shared/users.service";
 import {User} from "../shared/users.interface";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {UserModalComponent} from "../user-modal/user-modal.component";
 
 @Component({
-  selector: 'app-users-list',
-  standalone: true,
-  imports: [
-    TitleCasePipe,
-      CommonModule
-  ],
-  templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-users-list',
+    standalone: true,
+    imports: [
+        TitleCasePipe,
+        CommonModule,
+    ],
+    templateUrl: './users-list.component.html',
+    styleUrl: './users-list.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 
 
 })
 export class UsersListComponent {
 
-  usersSignal :Signal<Partial<User>[]>;
+    usersSignal: Signal<Partial<User>[]>;
+    private dialogRef: MatDialogRef<UserModalComponent> | null = null;
 
-  constructor(private usersService: UsersService) {
-    this.usersSignal  = toSignal(this.usersService.getUsers());
-  }
 
-  formatUrl(url: string): string {
-    return url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
-  }
+    constructor(private usersService: UsersService, private dialog: MatDialog) {
+        this.usersSignal = toSignal(this.usersService.getUsers());
+    }
+
+    formatUrl(url: string): string {
+        return url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
+    }
+
+    openEditUserModal(user: Partial<User>) {
+        if (this.dialogRef) {
+            this.dialogRef.close();
+        }
+
+        this.dialogRef = this.dialog.open(UserModalComponent, {
+            data: user,
+        });
+        this.dialogRef.afterClosed().subscribe(result => {
+            this.dialogRef.close();
+        })
+    }
 }
