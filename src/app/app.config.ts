1ee0a44a9e79c98265a7cabd7bbcae0314755b38
orBruhim <orbruhim@gmail.com>
2024-07-27 13:11:30 +0300

    add material settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import { provideAnimations} from "@angular/platform-browser/animations";
+import {MAT_DIALOG_DEFAULT_OPTIONS} from "@angular/material/dialog";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatNativeDateModule} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+    providers: [provideZoneChangeDetection({eventCoalescing: true}),
+        provideRouter(routes),
+        provideHttpClient(),
+        provideAnimations(),
+        {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}, provideAnimationsAsync(),
+        importProvidersFrom(MatNativeDateModule)
+    ]
 };
